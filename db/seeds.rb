5cf858113676e8c2f9fc91fbaf41644b3cf1de80
ryo <if14germany1248@icloud.com>
2021-05-01 18:09:28 +0900

    データの変更、追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,18 @@
+email = "test@example.com"
+password = "password"
+
 User.destroy_all
-Post.destroy_all
 
 user1 = User.create!(email: "koba@example.com", password: "password")
 user2 = User.create!(email: "gori@example.com", password: "password")
 user3 = User.create!(email: "mori@example.com", password: "password")
 
-Post.create!(content: "おはよう", user_id: user2.id)
-Post.create!(content: "こんにちは", user_id: user3.id)
-Post.create!(content: "こんばんは", user_id: user3.id)
+user2.Post.create!(content: "沖縄そば")
+user1.Post.create!(content: "海ぶどう")
+user3.Post.create!(content: "ゴーヤチャンプルー")
+user3.Post.create!(content: "サーターアンダーギー")
+user1.Post.create!(content: "タコライス")
+
+User.create!(email: email, password: password)
 
 puts "初期データの投入に成功しました！"
