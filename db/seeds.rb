ce2574deb2c89d07fb9038dae3f0065d3d94ad1f
ryo <if14germany1248@icloud.com>
2021-05-01 16:39:13 +0900

    関連付けの基礎と１対多の関連付け

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+
+user1 = User.create!(email: "koba@example.com", password: "password")
+user2 = User.create!(email: "gori@example.com", password: "password")
+user3 = User.create!(email: "mori@example.com", password: "password")
+
+Post.create!(content: "おはよう", user_id: user2.id)
+Post.create!(content: "こんにちは", user_id: user3.id)
+Post.create!(content: "こんばんは", user_id: user3.id)
+
+puts "初期データの投入に成功しました！"
