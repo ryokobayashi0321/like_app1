fbc41f13ad113d2df1fcbc25d5fa6eec0e2517af
ryo <if14germany1248@icloud.com>
2021-05-02 15:03:41 +0900

    多対多の関連付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   validates :content, presence: true
 end
