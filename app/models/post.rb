da691381c2b09de97e68f5e93a373290adf51c73
ryo <if14germany1248@icloud.com>
2021-05-02 15:46:55 +0900

    詳細ページに「いいね」機能を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   validates :content, presence: true
+
+  def liked_by?(user)
+    likes.find_by(user_id: user.id).present?
+  end
 end
