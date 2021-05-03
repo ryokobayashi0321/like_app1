cb780cdc39ea218b106a56ffac48129897f241fa
ryo <if14germany1248@icloud.com>
2021-05-03 11:08:50 +0900

    フラッシュ機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "posts#index"
-  resources :posts do
-    resource :likes, only: [:create, :destroy]
-  end
+  root "homes#index"
 end
